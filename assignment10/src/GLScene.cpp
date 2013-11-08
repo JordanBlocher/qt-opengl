dda46b5e9ae56b88676657be6d2fd959a47050f9
Henry Estela <henryestela@gmail.com>
2013-11-07 19:04:14 -0800

    Fixed timer not being started

--- assignment10/src/GLScene.cpp
+++ assignment10/src/GLScene.cpp
@@ -40,16 +40,24 @@ using namespace std;
 
 GLScene::GLScene(int width, int height, QWidget *parent, int argc, char* argv[]) : GLViewport(width, height, parent, NULL), background(QColor::fromRgbF(0.0, 0.0, 0.2)), font(Qt::white)
 {
-    this->setContextMenuPolicy(Qt::DefaultContextMenu);    
+    this->setContextMenuPolicy(Qt::DefaultContextMenu);  
+
+    for(int index=0; index < 12; index++)
+        this->keyHeld[index] = false;
+
+    // Start the timer
+    this->time = std::chrono::high_resolution_clock::now();
+    GLViewport::timer.start();  
 }
 
 void GLScene::initializeGL()
 {
-    GLViewport::initializeGL();
+   GLViewport::initializeGL();
 
     glEnable(GL_DEPTH_TEST);
     glDepthFunc(GL_LESS);
-    
+
+       /****** Deep GPU Stuff ******/
     //Shaders
     shared_ptr<GLShader> vertex(new GLShader(GL_VERTEX_SHADER, "vshader"));
     shared_ptr<GLShader> fragment(new GLShader(GL_FRAGMENT_SHADER, "fshader"));
@@ -91,14 +99,17 @@ void GLScene::initializeGL()
 
     //Set UBOs t Share
     cprogram->SetUBO(vertex_uniform);
+    cprogram->SetUBO(lights_uniform);
     cprogram->SetUBO(frag_uniform);
     tprogram->SetUBO(vertex_uniform);
-   
+    tprogram->SetUBO(lights_uniform);
+
     //Set Lighting
     std::shared_ptr<GLEmissive> lighting(new GLEmissive("lights"));
     lighting->ambient.color = glm::vec3(1.0f, 1.0f, 1.0f);
-    lighting->ambient.intensity = 0.4f;
+    lighting->ambient.intensity = 1.5f;
     this->AddToContext(lighting);
+
 
 
 }
@@ -172,8 +183,15 @@ void GLScene::paintGL()
 
 void GLScene::idleGL()
 {
+    // Timer
+    float dt;
+    time = chrono::high_resolution_clock::now();
+    dt = chrono::duration_cast< std::chrono::duration<float> >(time-this->start_time).count();
+    this->start_time = chrono::high_resolution_clock::now();
+
     GLViewport::updateGL();
     updateKeys();
+
 }
 
 void GLScene::resizeGL(int width, int height)
@@ -196,60 +214,50 @@ void GLScene::keyPressEvent(QKeyEvent *event)
     GLViewport::keyPressEvent(event);
     
     std::shared_ptr<GLEmissive> lighting = this->Get<GLEmissive>("lights");
-
-    // Dont let spammys happen
-    if(!event->isAutoRepeat())
+    // Act on the key press event
+    switch(event->key())
     {
-        // Act on the key press event
-        switch(event->key())
-        {
-            case (Qt::Key_Right):
-                keyHeld[8] = true;
-                break;    
-            case (Qt::Key_Left):
-                keyHeld[9] = true;
-                break;
-            case (Qt::Key_Up):
-                keyHeld[10] = true;
-                break;
-            case (Qt::Key_Down):
-                keyHeld[11] = true;
-                break;            
-            case (Qt::Key_Escape):
-                emit mainMenu(0);
-                break;
-           case(Qt::Key_1):
-                lighting->ambient.intensity +=0.05f;
-                break;
-            case(Qt::Key_2):
-                lighting->ambient.intensity -=0.05f;
-                break;
-        }
+        case (Qt::Key_Right):
+            keyHeld[8] = true;
+            break;    
+        case (Qt::Key_Left):
+            keyHeld[9] = true;
+            break;
+        case (Qt::Key_Up):
+            keyHeld[10] = true;
+            break;
+        case (Qt::Key_Down):
+            keyHeld[11] = true;
+            break;            
+        case (Qt::Key_Escape):
+            emit mainMenu(0);
+            break;
+       case(Qt::Key_1):
+            lighting->ambient.intensity +=0.05f;
+            break;
+        case(Qt::Key_2):
+            lighting->ambient.intensity -=0.05f;
+            break;
     }
-
 }
 
 void GLScene::keyReleaseEvent(QKeyEvent *event)
 {
-    // Dont let spammys happen
-    if(!event->isAutoRepeat())
+    // Act on the key press event
+    switch(event->key())
     {
-        // Act on the key press event
-        switch(event->key())
-        {
-            case (Qt::Key_Right):
-                keyHeld[8] = false;
-                break;    
-            case (Qt::Key_Left):
-                keyHeld[9] = false;
-                break;
-            case (Qt::Key_Up):
-                keyHeld[10] = false;
-                break;
-            case (Qt::Key_Down):
-                keyHeld[11] = false;
-                break;            
-        }
+        case (Qt::Key_Right):
+            keyHeld[8] = false;
+            break;    
+        case (Qt::Key_Left):
+            keyHeld[9] = false;
+            break;
+        case (Qt::Key_Up):
+            keyHeld[10] = false;
+            break;
+        case (Qt::Key_Down):
+            keyHeld[11] = false;
+            break;            
     }
 }
 
